ba3bcbef9949793c0d5d9769dceae7294f2fd391
orcunasik <asikorcun@gmail.com>
2021-03-18 00:07:53 +0300

    Sepet Operasyonu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
+import { FilterPipePipe } from './pipes/filter-pipe.pipe'
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +11,10 @@ import { CategoryComponent } from './components/category/category.component';
 import { NaviComponent } from './components/navi/navi.component';
 import { VatAddedPipe } from './pipes/vat-added.pipe';
 import { FormsModule } from '@angular/forms';
-import { FilterPipePipe } from './pipes/filter-pipe.pipe'
+
+import {ToastrModule} from 'ngx-toastr';
+import { CartSummaryComponent } from './components/cart-summary/cart-summary.component'
+
 
 @NgModule({
   declarations: [
@@ -18,13 +23,18 @@ import { FilterPipePipe } from './pipes/filter-pipe.pipe'
     CategoryComponent,
     NaviComponent,
     VatAddedPipe,
-    FilterPipePipe
+    FilterPipePipe,
+    CartSummaryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ToastrModule.forRoot({
+      positionClass:"toast-bottom-right"
+    })
     
   ],
   providers: [],
